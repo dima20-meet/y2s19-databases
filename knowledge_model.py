78930395c2926bce643edff9e33774f74ad9b571
myname21-meet <myname21@meet.mit.edu>
2019-07-29 03:28:33 -0400

    databaseslab1

--- knowledge_model.py
+++ knowledge_model.py
@@ -4,8 +4,6 @@ from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
 
 Base = declarative_base()
-
-class Knowledge(Base):
 	# Create a table with 4 columns
 	# The first column will be the primary key
 	# The second column should be a string representing
@@ -14,4 +12,20 @@ class Knowledge(Base):
 	# topic of the article. The last column will be
 	# an integer, representing your rating of the article.
 
-	pass
+class Knowledge(Base):
+    	__tablename__ = 'Articles'
+    	Article_name = Column(String, primary_key=True)
+    	year = Column(Integer)
+    	rating = Column(String)
+
+    	def __repr__(self):
+    		return ("If you want to learn about {}, you should look at the Wikipedia article called {}. We gave this article a rating of {}").format(self.Article_name, self.year, self.rating)
+    		
+
+BHM= Knowledge(Article_name="Black History Month", year=2019, rating="10 out of 10!")
+print(BHM)
+Vaccination= Knowledge(Article_name="Vaccination", year=2019, rating="8 out of 10!")
+print(Vaccination)
+# Fast Food= Knowledge(Article_name="Fast Food", year=2019, rating=7/10)
+# print(Fast Food)
+
